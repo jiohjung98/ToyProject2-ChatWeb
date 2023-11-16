c38f694c9f33894bd36ea8fed622f68058cab69e
jiohjung98 <ckaquz98@naver.com>
2023-11-16 16:41:53 +0900

    feat: fixed 1:1chat

--- src/app/userSelect/page.tsx
+++ src/app/userSelect/page.tsx
@@ -68,52 +68,59 @@ function UserSelect() {
     // useEffect(() => {
     //     getMyChats()
     //  }, []);
+
+    
        
 
     const handleChatClick = async () => {
         if (selectedUsers.length === 1) {
             const selectedUser = selectedUsers[0];
+    
             try {
                 const res = await instance.get<unknown, ResponseBody>(`chat`);
                 if (res) {
-                  const { chats } = res;
-                  const chatName = `1:1 Chat with ${selectedUser.name}`;
-                  const existingChat = chats ? chats.find((chat) => chat.name === chatName) : null;
+                    const { chats } = res;
+    
+                    const sortedUserIds = [userId, selectedUser.id].sort();
+                    const chatName = `1:1 Chat ${sortedUserIds.join('_')}`;
 
+                    console.log(chatName)
+    
+                    const existingChat = chats ? chats.find((chat) => chat.name === chatName) : null;
+    
                     if (existingChat) {
                         // 이미 존재하는 채팅방으로 이동
-                        enterChatRoom(existingChat);
-                        console.log('이미 채팅방이 존재해요. 그 채팅방으로 이동하겠습니다.')
-                        console.log(existingChat)
-                        console.log(userId)
-                        console.log(selectedUser.id)
-                        console.log(selectedUser.name)
-        
+                        // enterChatRoom(existingChat);
+                        console.log('이미 채팅방이 존재해요. 그 채팅방으로 이동하겠습니다.');
+                        console.log(existingChat);
+                        console.log(userId);
+                        console.log(selectedUser.id);
+                        console.log(selectedUser.name);
                     } else {
                         // 채팅방이 없으면 채팅방 생성 후 이동
-                        console.log('채팅방이 없습니다. 새로운 채팅방을 생성하겠습니다.')
+                        console.log('채팅방이 없습니다. 새로운 채팅방을 생성하겠습니다.');
                         const isPrivate = true;
-                        console.log(selectedUser)
-                        console.log(selectedUser.id)
-                        console.log(selectedUser.name)
-        
-                        console.log(myChats)
-                        console.log(existingChat)
-                        createChatRoom(chatName, isPrivate)
-                        console.log(userId)
+                        console.log(selectedUser);
+                        console.log(selectedUser.id);
+                        console.log(selectedUser.name);
+    
+                        console.log(myChats);
+                        console.log(existingChat);
+                        // createChatRoom(chatName, isPrivate);
+                        console.log(userId);
                     }
                 } else {
                     console.error('선택된 사용자의 ID가 없습니다.');
-                    console.log(userId)
+                    console.log(userId);
                 }
             } catch (error) {
                 console.error(error);
             }
-        }
-        else {
+        } else {
             setShowModal(true);
         }
     };
+    
     
     // 모달 닫기
     const handleCloseModal = () => {
@@ -181,7 +188,7 @@ function UserSelect() {
     const getUsers = async () => {
         try {
             let res = await instance.get<unknown, User[]>('/users');
-            res = res.filter((user) => user.id !== sessionStorage.getItem('userId'));
+            res = res.filter((user) => user.id !== localStorage.getItem('userId'));
             setUsers(res);
             setLoading(false);
         } catch (error) {
